8cc37c759f671060ce6920886315a1b67ece9471
Zinx140 <frostocean4@gmail.com>
2025-02-10 22:41:33 +0700

    two way bubble sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
+#include <cmath>
 
 using namespace std;
 
@@ -19,27 +20,27 @@ int main()
         cout << ">> ";
         cin >> menuInp;
 
-        switch (menuInp) {
-            case 1:
+            if (menuInp == 1) {
                 system("cls");
 
                 system("pause");
-                break;
-            case 2:
+            } else if (menuInp == 2) {
                 system("cls");
 
                 system("pause");
-                break;
-            case 3:
+            } else if (menuInp == 3) {
                 system("cls");
 
                 system("pause");
-                break;
-            case 4:
+            } else if (menuInp == 4) {
                 system("cls");
                 int arr[] = {10, 5, 7, 1, 3, 100};
                 int n = sizeof(arr)/sizeof(int);
                 cout << n << endl;
+                for (int i = 0; i < n; i++) {
+                    cout << arr[i] << " ";
+                }
+
                 for (int i = 0; i < n / 2 + 1; i++) {
                     for (int j = 0; j < n - i - 1; j++) {
                         if (arr[j] > arr[j + 1]) {
@@ -50,17 +51,45 @@ int main()
                     }
                 }
 
+                cout << endl;
                 for (int i = 0; i < n; i++) {
                     cout << arr[i] << " ";
                 }
+                cout << endl;
                 system("pause");
-                break;
-            case 5:
+            } else if (menuInp == 5) {
                 system("cls");
+                int arr[] = {10, 5, 7, 1, 3, 4, 50};
+                int n = sizeof(arr)/sizeof(int);
+                for (int i = 0; i < n; i++) {
+                    cout << arr[i] << " ";
+                }
+
+                for (int i = 0; i < sqrt(n); i++) {
+                    for (int j = i; j < n - i - 1; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    for (int j = n - i - 1; j > i; j--) {
+                        if (arr[j] < arr[j - 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j - 1];
+                            arr[j - 1] = temp;
+                        }
+                    }
+                }
+
+                cout << endl;
+                for (int i = 0; i < n; i++) {
+                    cout << arr[i] << " ";
+                }
+                cout << endl;
 
                 system("pause");
-                break;
-        }
+            }
     } while (menuInp != 6);
 
     return 0;
