1983a21f7741848ac22dc855baa9965923128f9c
Zinx140 <frostocean4@gmail.com>
2025-02-10 10:18:13 +0700

    bubble sort

--- main.cpp
+++ main.cpp
@@ -37,7 +37,22 @@ int main()
                 break;
             case 4:
                 system("cls");
+                int arr[] = {10, 5, 7, 1, 3, 100};
+                int n = sizeof(arr)/sizeof(int);
+                cout << n << endl;
+                for (int i = 0; i < n / 2 + 1; i++) {
+                    for (int j = 0; j < n - i - 1; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
 
+                for (int i = 0; i < n; i++) {
+                    cout << arr[i] << " ";
+                }
                 system("pause");
                 break;
             case 5:
